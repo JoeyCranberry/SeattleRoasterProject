498e4c7478ee3d3ea98c47136b666bf1edfab0ef
JoeyCranberry <carberry.joey@gmail.com>
2022-11-09 15:11:30 -0800

    Finished Parsing for Anchorhead
    Added parsing for grabbing all Anchorhead product items

--- RoasterSiteDataScrapper/Parsers/AnchorheadParser.cs
+++ RoasterSiteDataScrapper/Parsers/AnchorheadParser.cs
@@ -9,30 +9,75 @@ namespace RoasterBeansDataAccess.Parsers
 {
 	public static class AnchorheadParser
 	{
-		public static List<BeanListing> ParseBeans(HtmlDocument shopHTML)
-		{
-			var shopItems = shopHTML.DocumentNode.Descendants()
-				.Where(parentNode => parentNode.GetAttributeValue("class", "").Contains("product-grid-item"))
-				.ToList();
+		private const string baseURL = "https://anchorheadcoffee.com";
+		private static List<string> excludedTerms = new List<string>{ "choice", "sample", "tumbler", "shirt", "tee", "glass", "beanie", "gift card", "anchorhead - coffee supply co" };
 
-			// collection__products
-			// product-grid-item 
+		public static List<BeanListing> ParseBeans(HtmlDocument shopHTML, Roaster roaster)
+		{
+			HtmlNode shopParent = shopHTML.DocumentNode.SelectSingleNode("//div[contains(@class, 'collection__products')]").FirstChild;
+			List<HtmlNode> shopItems = shopParent.SelectNodes("//div[contains(@class, 'product-grid-item')]").ToList();
+			shopItems.RemoveAll(child => child.ChildNodes.Count == 0);
 
 			List<BeanListing> listings = new List<BeanListing>();
 			
-			foreach (var item in shopItems)
+			foreach (HtmlNode productListing in shopItems)
 			{
 				BeanListing listing = new BeanListing();
-				var imageNode = item.FirstChild;
-				string imageUrl = imageNode.Descendants()
-					.Where(node => node.GetAttributeValue("class", "").Contains("collection__image__top"))
-					.First().GetAttributeValue("style", "");
-				imageUrl = imageUrl.Substring(imageUrl.IndexOf("url("), imageUrl.IndexOf(");"));
-				listing.ImageURL = imageUrl;
+
+				HtmlNode productImage = productListing.SelectSingleNode(".//a[contains(@class, 'lazy-image')]");
+				string productURL = baseURL + productImage.GetAttributeValue("href", "");
+				string imageURL = productImage.ChildNodes[1].GetAttributeValue("style", "");
+				imageURL = imageURL
+					.Replace("background-image: url(&quot;", "")
+					.Replace("&quot;);", "");
+
+				// Check for lazy loaded image
+				if(String.IsNullOrEmpty(imageURL)) 
+                {
+					imageURL = "https://" + productImage.GetAttributeValue("style", "")
+						.Replace("padding-top:100%;", "")
+						.Replace("background-image:  url('//", "")
+						.Replace("_1x1", "_360x")
+						.Replace("');", "").Trim();
+				}
+
+				listing.ProductURL = productURL;
+				listing.ImageURL = imageURL;
+
+				string name = productListing.SelectSingleNode(".//p[contains(@class, 'product__grid__title')]").InnerText.Replace("\n", "").Trim();
+				HtmlNode priceNode = productListing.SelectSingleNode(".//span[contains(@class, 'price')]");
+				string price = priceNode.InnerText.Replace("From ", "").Replace("\n", "").Replace("$", "").Trim();
+
+				decimal parsedPrice;
+				if (Decimal.TryParse(price, out parsedPrice))
+				{
+					listing.PriceBeforeShipping = parsedPrice;
+				}
+				listing.FullName = name;
+
+				listing.DateAdded = DateTime.Now;
+				listing.RoasterId = roaster.RoasterId;
+
+				listing.SetOriginsFromName();
+				listing.SetProcessFromName();
+				listing.SetDecafFromName();
+
+				listings.Add(listing);
 			}
 
+			// Remove any excluded terms
+			foreach(var product in listings)
+            {
+				foreach(string term in excludedTerms)
+                {
+					if(product.FullName.ToLower().Contains(term))
+                    {
+						product.IsExcluded = true;
+					}
+                }
+            }
+
 			return listings;
-		}
-		// product-grid-item 
+		}	
 	}
 }
