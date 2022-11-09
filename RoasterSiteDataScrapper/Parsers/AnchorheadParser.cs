3a79c99dced460008925698c87bb169ff01264e8
JoeyCranberry <carberry.joey@gmail.com>
2022-11-08 18:33:46 -0800

    Started parsing for Anchorhead Shop page

---
+++ RoasterSiteDataScrapper/Parsers/AnchorheadParser.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoasterBeansDataAccess.Parsers
+{
+	public static class AnchorheadParser
+	{
+		public static List<BeanListing> ParseBeans(HtmlDocument shopHTML)
+		{
+			var shopItems = shopHTML.DocumentNode.Descendants()
+				.Where(parentNode => parentNode.GetAttributeValue("class", "").Contains("product-grid-item"))
+				.ToList();
+
+			// collection__products
+			// product-grid-item 
+
+			List<BeanListing> listings = new List<BeanListing>();
+			
+			foreach (var item in shopItems)
+			{
+				BeanListing listing = new BeanListing();
+				var imageNode = item.FirstChild;
+				string imageUrl = imageNode.Descendants()
+					.Where(node => node.GetAttributeValue("class", "").Contains("collection__image__top"))
+					.First().GetAttributeValue("style", "");
+				imageUrl = imageUrl.Substring(imageUrl.IndexOf("url("), imageUrl.IndexOf(");"));
+				listing.ImageURL = imageUrl;
+			}
+
+			return listings;
+		}
+		// product-grid-item 
+	}
+}
