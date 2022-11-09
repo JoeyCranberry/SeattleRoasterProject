d7e8549bf993efb1b026fb4e202b2cf7569a1c29
JoeyCranberry <carberry.joey@gmail.com>
2022-11-09 15:11:50 -0800

    Added page for Beans

--- SeattleRoasterProject/Data/Services/BeanService.cs
+++ SeattleRoasterProject/Data/Services/BeanService.cs
@@ -4,9 +4,15 @@ namespace SeattleRoasterProject.Data.Services
 {
 	public class BeanService
 	{
-		public async Task GetBeans(Roaster roaster)
+		public async Task<List<BeanListing>> GetBeans(Roaster roaster)
 		{
-			await BeanDataScraper.ScrapeBeans(roaster);
+			await BeanDataScraper.UpdateRoasterBeanListing(roaster);
+			return await BeanDataScraper.GetRoasterBeans(roaster);
+		}
+
+		public async Task<bool> CheckForUpdate(Roaster roaster)
+        {
+			return await BeanDataScraper.UpdateRoasterBeanListing(roaster);
 		}
 	}
 }
