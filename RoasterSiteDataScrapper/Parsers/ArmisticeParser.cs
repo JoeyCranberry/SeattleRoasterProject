3174492b1caa4f92bca9971015146de6006580a7
JoeyCranberry <carberry.joey@gmail.com>
2022-11-12 22:03:43 -0800

    Created parser for Armistice

---
+++ RoasterSiteDataScrapper/Parsers/ArmisticeParser.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using RoasterBeansDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoasterBeansDataAccess.Parsers
+{
+	public static class ArmisticeParser
+	{
+		private static List<string> excludedTerms = new List<string> { "mug", "thermos" };
+
+		public static List<BeanModel> ParseBeans(HtmlDocument shopHTML, RoasterModel roaster)
+		{
+			HtmlNode shopParent = shopHTML.DocumentNode.SelectSingleNode("//ul[contains(@class, 'products')]");
+			List<HtmlNode> shopItems = shopParent.SelectNodes(".//li").ToList();
+
+			List<BeanModel> listings = new List<BeanModel>();
+
+			foreach (HtmlNode productListing in shopItems)
+			{
+				BeanModel listing = new BeanModel();
+
+				string imageURL = productListing.SelectSingleNode(".//img").GetAttributeValue("src", "");
+				string productURL = productListing.SelectSingleNode(".//a").GetAttributeValue("href", "");
+
+				listing.ProductURL = productURL;
+				listing.ImageURL = imageURL;
+
+				string name = productListing.SelectSingleNode(".//h2").InnerText.Trim();
+				listing.FullName = name;
+
+				string price = productListing.SelectSingleNode(".//bdi").ChildNodes[1].InnerText.Trim();
+				decimal parsedPrice;
+				if (Decimal.TryParse(price, out parsedPrice))
+				{
+					listing.PriceBeforeShipping = parsedPrice;
+				}
+
+				listing.AvailablePreground = true;
+				listing.SetOriginsFromName();
+				listing.SetProcessFromName();
+				listing.SetDecafFromName();
+				listing.SetOrganicFromName();
+
+				listing.MongoRoasterId = roaster.Id;
+				listing.RoasterId = roaster.RoasterId;
+
+				listings.Add(listing);
+			}
+
+			// Remove any excluded terms
+			foreach (var product in listings)
+			{
+				foreach (string term in excludedTerms)
+				{
+					if (product.FullName.ToLower().Contains(term))
+					{
+						product.IsExcluded = true;
+					}
+				}
+			}
+
+			return listings;
+		}
+
+	}
+}
