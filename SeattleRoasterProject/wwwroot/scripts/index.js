9cf70d1499fa5f941797a4ec8589e32c25724e07
JoeyCranberry <carberry.joey@gmail.com>
2022-11-16 20:34:44 -0800

    Added Show/Hide for toasts

--- SeattleRoasterProject/wwwroot/scripts/index.js
+++ SeattleRoasterProject/wwwroot/scripts/index.js
@@ -8,3 +8,25 @@ function ShowModal(elementId) {
 function HideModal(elementId) {
     $(elementId).modal('hide');
 }
+
+const toastTrigger = document.getElementById('liveToastBtn')
+
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', () => {
+        
+    })
+}
+
+function ShowNewToast(elementId) {
+    const toastToShow = document.getElementById(elementId)
+    const toast = new bootstrap.Toast(toastToShow)
+
+    toast.show()
+}
+
+function HideToast(elementId) {
+    const toastToShow = document.getElementById(elementId)
+    const toast = new bootstrap.Toast(toastToShow)
+
+    toast.hide()
+}
