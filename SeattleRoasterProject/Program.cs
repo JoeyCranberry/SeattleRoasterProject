b186d56392451e1611b78b47ac5f330bd81df721
JoeyCranberry <carberry.joey@gmail.com>
2022-12-07 13:31:49 -0800

    Removed Default Blazor files

--- SeattleRoasterProject/Program.cs
+++ SeattleRoasterProject/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<RoasterService>();
 builder.Services.AddSingleton<BeanService>();
 builder.Services.AddSingleton<BeanFilterService>();
