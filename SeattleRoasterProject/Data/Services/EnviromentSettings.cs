5014acd413d88a1d5a7edeacfb76d777218d8d69
JoeyCranberry <carberry.joey@gmail.com>
2022-12-09 16:48:32 -0800

    New service for accessing app settings

---
+++ SeattleRoasterProject/Data/Services/EnviromentSettings.cs
@@ -0,0 +1,35 @@
+﻿namespace SeattleRoasterProject.Data.Services
+{
+	public class EnviromentSettings
+	{
+		public bool ShowProductionInvisible { get; set; } = false;
+		public string EnviromentName { get; set; } = "Production";
+
+		public enum Enviroment
+		{ 
+			DEVELOPMENT,
+			STAGING,
+			PRODUCTION
+		}
+
+		public Enviroment GetEnviroment(IConfiguration config)
+		{
+			var envString = config.GetValue<string>("Enviroment", "Unknown");
+			switch (envString)
+			{
+				case "Development":
+					return Enviroment.DEVELOPMENT;
+				case "Staging":
+					return Enviroment.STAGING;
+				case "Production":
+				default:
+					return Enviroment.PRODUCTION;
+			}
+		}
+
+		public bool GetShowProductionInvisible(IConfiguration config)
+		{
+			return config.GetValue<bool>("ShowProductionInvisible", false);
+		}
+	}
+}
