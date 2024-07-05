5d19b98c572a62c6faf1b89a9089d07a59d426c3
Joe Carberry <34722148+JoeyCranberry@users.noreply.github.com>
2024-07-05 13:22:41 -0700

    Injected Env Settings in Bean Service

--- SeattleRoasterProject/Data/Services/BeanService.cs
+++ SeattleRoasterProject/Data/Services/BeanService.cs
@@ -1,5 +1,4 @@
-﻿using RoasterBeansDataAccess;
-using RoasterBeansDataAccess.Models;
+﻿using RoasterBeansDataAccess.Models;
 using RoasterBeansDataAccess.DataAccess;
 using System.Net;
 
@@ -7,19 +6,23 @@ namespace SeattleRoasterProject.Data.Services
 {
     public class BeanService
 	{
-		public async Task<List<BeanModel>> GetAllBeans(EnvironmentSettings.Environment env)
+		private readonly EnvironmentSettings _environmentSettings;
+		private readonly bool _isDevelopment = false;
+
+		public BeanService(EnvironmentSettings environmentSettings)
 		{
-			return await BeanAccess.GetAllBeans(env == EnvironmentSettings.Environment.Development);
+			_environmentSettings = environmentSettings;
+			_isDevelopment = environmentSettings.GetEnvironment() == EnvironmentSettings.Environment.Development;
 		}
 
-		public async Task<List<BeanModel>> GetAllBeansWithBrokenImageLink(EnvironmentSettings.Environment env)
+		public async Task<List<BeanModel>> GetAllBeans()
 		{
-			var allBeans = await BeanAccess.GetAllBeansNotExcluded(env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.GetAllBeans(_isDevelopment);
+		}
 
-			if(allBeans == null) 
-			{
-				return new();
-			}
+		public async Task<List<BeanModel>> GetAllBeansWithBrokenImageLink()
+		{
+			var allBeans = await BeanAccess.GetAllBeansNotExcluded(_isDevelopment);
 
 			var beansWithBrokenImageLink = new List<BeanModel>();
 
@@ -39,7 +42,7 @@ namespace SeattleRoasterProject.Data.Services
 			try
 			{
 				//Creating the HttpWebRequest
-				HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+				var request = WebRequest.Create(url) as HttpWebRequest;
 				//Setting the Request method HEAD, you can also use GET too.
 				request.Method = "HEAD";
 				//Getting the Web Response.
@@ -55,45 +58,45 @@ namespace SeattleRoasterProject.Data.Services
 			}
 		}
 
-		public async Task<BeanGetResult> GetBeansByFilter(BeanFilter filter, EnvironmentSettings.Environment env)
+		public async Task<BeanGetResult> GetBeansByFilter(BeanFilter filter)
 		{
-			return await BeanAccess.GetBeansByFilter(filter, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.GetBeansByFilter(filter, _isDevelopment);
 		}
 
-		public async Task<List<BeanModel>> GetBeansByIds(List<string> beanIds, EnvironmentSettings.Environment env)
+		public async Task<List<BeanModel>> GetBeansByIds(List<string> beanIds)
 		{
-			return await BeanAccess.GetAllBeansByIds(beanIds, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.GetAllBeansByIds(beanIds, _isDevelopment);
 		}
 
-		public async Task<List<BeanModel>> GetAllProductionInvisibleBeans(EnvironmentSettings.Environment env)
+		public async Task<List<BeanModel>> GetAllProductionInvisibleBeans()
 		{
-			return await BeanAccess.GetAllProductionInvisibleBeans(env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.GetAllProductionInvisibleBeans(_isDevelopment);
 		}
 
-		public async Task<bool> AddBeanToDb(BeanModel newBean, EnvironmentSettings.Environment env)
+		public async Task<bool> AddBeanToDb(BeanModel newBean)
         {
-			return await BeanAccess.AddBean(newBean, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.AddBean(newBean, _isDevelopment);
         }
 
-		public async Task<bool> UpdateExistingBean(BeanModel editBean, EnvironmentSettings.Environment env)
+		public async Task<bool> UpdateExistingBean(BeanModel editBean)
         {
-			return await BeanAccess.UpdateBean(editBean, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.UpdateBean(editBean, _isDevelopment);
         }
 
-		public async Task<bool> SetBeanToInactive(BeanModel bean, EnvironmentSettings.Environment env)
+		public async Task<bool> SetBeanToInactive(BeanModel bean)
 		{
 			bean.IsActiveListing = false;
-			return await BeanAccess.UpdateBean(bean, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.UpdateBean(bean, _isDevelopment);
 		}
 
-		public async Task<bool> DeleteBean(BeanModel delBean, EnvironmentSettings.Environment env)
+		public async Task<bool> DeleteBean(BeanModel delBean)
         {
-			return await BeanAccess.DeleteBean(delBean, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.DeleteBean(delBean, _isDevelopment);
 		}
 
-		public async Task<bool> UnsetField(string fieldName, EnvironmentSettings.Environment env)
+		public async Task<bool> UnsetField(string fieldName)
 		{
-			return await BeanAccess.UnsetField(fieldName, env == EnvironmentSettings.Environment.Development);
+			return await BeanAccess.UnsetField(fieldName, _isDevelopment);
 		}
 	}
 
