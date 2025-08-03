d8b36d8560a1253b78be2e04aefec78fe48308a9
Joe Carberry <34722148+JoeyCranberry@users.noreply.github.com>
2025-08-03 16:37:22 -0700

    Updated Nav and finished up Beans page

--- RoasterSiteDataScrapper/Models/RoasterModel.cs
+++ RoasterSiteDataScrapper/Models/RoasterModel.cs
@@ -8,15 +8,15 @@ public class RoasterModel
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 
     public int RoasterId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public RoasterLocation Location { get; set; }
-    public string ShopURL { get; set; }
-    public string ImageURL { get; set; }
-    public string ImageClass { get; set; }
+    public string ShopURL { get; set; } = string.Empty;
+    public string ImageURL { get; set; } = string.Empty;
+    public string ImageClass { get; set; } = string.Empty;
     public int FoundingYear { get; set; }
     public bool IsExcluded { get; set; } = false;
     public bool ContactedForPermission { get; set; } = false;
